12a01b1e7dff05b0d6cb9110797c3cc460082f81
Yannick Battail <yannick.battail@gmail.com>
2014-02-20 22:10:05 +0100

    add char rang and embed quantifier

--- webPage/schema.js
+++ webPage/schema.js
@@ -14,6 +14,16 @@ function getNodeNum(selectedNode) {
     return t[1];
 }
 
+function showQuantifier(event) {
+    var n = getNodeNum(event.target);
+    var quantifier = gel('quantifier-' + n);
+    if (quantifier.style.display == 'inline') {
+        quantifier.style.display = 'none';
+    } else {
+        quantifier.style.display = 'inline';
+    }
+}
+
 function refesh() {
     buildRegexp();
     testRegexp();
@@ -52,44 +62,21 @@ function buildRegexpRecurs(nodeRoot) {
             var v2 = gel('value2-' + n) ? getValue('value2-' + n) : null;
             if (gel('type-' + n).value == "text") {
                 regexp += escapeCharFactor(v1);
-            } else if (gel('type-' + n).value == "quantifier") {
-                if (!v1) {
-                    v1 = 0;
-                }
-                if (v1 == 0 && v2 == 1) {
-                    regexp += '?';
-                } else if (v1 == 0 && v2 == '') {
-                    regexp += '*';
-                } else if (v1 == 1 && v2 == '') {
-                    regexp += '+';
-                } else if (v1 == v2) {
-                    regexp += '{' + v1 + '}';
-                } else {
-                    regexp += '{' + v1 + ',' + v2 + '}';
-                }
-            } else if (gel('type-' + n).value == "charClass") {
+                regexp += buildQuantifier(getValue('min-' + n), getValue('max-' + n));
+            } else if (gel('type-' + n).value == "class") {
                 regexp += '[';
-                if (v2) {
+                if (v1) {
                     regexp += '^';
                 }
-                regexp += escapeCharClass(v1);
+                regexp += buildClass(gel('group-' + n));
                 regexp += ']';
-            } else if (gel('type-' + n).value == "predefinedClass") {
-                var r = '';
-                if (v2) {
-                    if ((v1 == '\\d') || (v1 == '\\s') || (v1 == '\\w')) {
-                        r = v1.toUpperCase();
-                    } else {
-                        r += '^' + v1 + '';
-                    }
-                } else {
-                    r = v1;
-                }
-                regexp += '[' + r + ']';
-            } else if (gel('type-' + n).value == "charRange") {
-                regexp += '[' + v1 + '-' + v2 + ']';
+                regexp += buildQuantifier(getValue('min-' + n), getValue('max-' + n));
             } else if (gel('type-' + n).value == "or") {
-                regexp += '|';
+                regexp += '(';
+                regexp += buildRegexpRecurs(gel('group-' + n));
+                regexp += ')|(';
+                regexp += buildRegexpRecurs(gel('group2-' + n));
+                regexp += ')';
             } else if (gel('type-' + n).value == "group") {
                 regexp += '(';
                 if (v1) {
@@ -97,8 +84,51 @@ function buildRegexpRecurs(nodeRoot) {
                 }
                 regexp += buildRegexpRecurs(gel('group-' + n));
                 regexp += ')';
+                regexp += buildQuantifier(getValue('min-' + n), getValue('max-' + n));
             }
         }
+    }
+    return regexp;
+}
+
+function buildClass(nodeRoot) {
+    var nodes = nodeRoot.childNodes;
+    var regexp = '';
+    for (var i = 0; i < nodes.length; i++) {
+        var node = nodes[i];
+        if ((node.nodeType == Node.ELEMENT_NODE) && (node.className == 'nodeClass')) {
+            var n = getNodeNum(node);
+            var v1 = gel('value-' + n) ? getValue('value-' + n) : null;
+            var v2 = gel('value2-' + n) ? getValue('value2-' + n) : null;
+            if (gel('type-' + n).value == "charClass") {
+                regexp += escapeCharClass(v1);
+            } else if (gel('type-' + n).value == "predefinedClass") {
+                regexp += v1;
+            } else if (gel('type-' + n).value == "charRange") {
+                regexp += v1 + '-' + v2;
+            }
+        }
+    }
+    return regexp;
+}
+
+function buildQuantifier(v1, v2) {
+    var regexp = '';
+    if (!v1) {
+        v1 = 0;
+    }
+    if (v1 == 0 && v2 == 1) {
+        regexp += '?';
+    } else if (v1 == 0 && v2 == '') {
+        regexp += '*';
+    } else if (v1 == 1 && v2 == '') {
+        regexp += '+';
+    } else if (v1 == 1 && v2 == 1) {
+        regexp += '';
+    } else if (v1 == v2) {
+        regexp += '{' + v1 + '}';
+    } else {
+        regexp += '{' + v1 + ',' + v2 + '}';
     }
     return regexp;
 }
@@ -114,8 +144,10 @@ function testRegexp() {
             gel('result').value = res.join('\n');
             gel('errMsg').innerHTML = '';
         }
+        gel('regexp').className = 'regexpOk';
     } catch (e) {
         gel('errMsg').innerHTML = 'Some thing is wrong in the regexp. Error message: ' + e.message;
+        gel('regexp').className = 'regexpFailed';
     }
     
 }
@@ -157,22 +189,29 @@ document.addEventListener("dragover", function(event) {
 
 document.addEventListener("dragenter", function(event) {
     // highlight potential drop target when the draggable element enters it
-    if (event.target.className == "group") {
-        event.target.style.background = "chartreuse";
-    }
-    if (event.target.parentNode.className == "group") {
-        event.target.style.background = "chartreuse";
-    }
+    var source = dragged.parentNode;
+    var dest = event.target;
+    var elem = dragged;
     
+    // move dragged elem to the selected drop target
+    if (isValidMove(source, dest, elem)) {
+        if (dest.style) {
+            dest.style.background = "chartreuse";
+        }
+    } else {
+        if (dest.style) {
+            dest.style.background = "red";
+        }
+    }
 }, false);
 
 document.addEventListener("dragleave", function(event) {
     // reset background of potential drop target when the draggable element leaves it
-    if (event.target.className == "group") {
-        event.target.style.background = "";
-    }
-    if (event.target.parentNode.className == "group") {
-        event.target.style.background = "";
+    // var source = dragged.parentNode;
+    var dest = event.target;
+    // var elem = dragged;
+    if (dest.style) {
+        dest.style.background = "";
     }
     
 }, false);
@@ -183,37 +222,61 @@ document.addEventListener("drop", function(event) {
     var source = dragged.parentNode;
     var dest = event.target;
     var elem = dragged;
-    dest.style.background = "";
-    
-    if (dest == elem) {
-        // dragged in the same node
-        return;
+    if (dest.style) {
+        dest.style.background = "";
     }
-    
-    if ((dest.id == 'ressources') || (dest.parentNode.id == "ressources") || (dest.parentNode.parentNode.id == "ressources")) {
-        return;
-    }
-    
     // move dragged elem to the selected drop target
-    if ((dest.className == "group") || (dest.parentNode.className == "group")) {
-        if (source.id == 'ressources') {
-            var newNode = elem.cloneNode(true);
-            currentId++;
-            newNode.innerHTML = newNode.innerHTML.replace(new RegExp('XXX', 'g'), String(currentId));
-            newNode.id = newNode.id.replace('XXX', String(currentId));
-            newNode.setAttribute("style", "");
-            elem = newNode;
-        } else {
-            source.removeChild(elem);
-        }
-        if (dest.id != 'recycledBin') {
-            if (dest.parentNode.className == "group") {
-                dest.parentNode.insertBefore(elem, dest);
-            } else {
-                dest.appendChild(elem);
-            }
-        }
-        refesh();
+    if (isValidMove(source, dest, elem)) {
+        doMove(source, dest, elem);
     }
     
 }, false);
+
+function isValidMove(source, dest, elem) {
+    if (dest == elem) {
+        // dragged in the same node
+        return false;
+    }
+    if ((dest.id == 'ressources') || (dest.parentNode.id == "ressources") || (dest.parentNode.parentNode.id == "ressources")) {
+        return false;
+    }
+    // move dragged elem to the selected drop target
+    if (dest.id == 'recycledBin') {
+        if (source.id == "ressources") {
+            return false;
+        }
+        return true;
+    }
+    if ((dest.className == "group") || (dest.parentNode.className == "group")) {
+        if (elem.className == "node") {
+            return true;
+        }
+    } else if ((dest.className == "groupClass") || (dest.parentNode.className == "groupClass")) {
+        if (elem.className == "nodeClass") {
+            return true;
+        }
+    }
+    return false;
+}
+
+function doMove(source, dest, elem) {
+    if (source.id == 'ressources') {
+        var newNode = elem.cloneNode(true);
+        currentId++;
+        newNode.innerHTML = newNode.innerHTML.replace(new RegExp('XXX', 'g'), String(currentId));
+        newNode.innerHTML = newNode.innerHTML.replace('disabled="disabled"', '');
+        newNode.id = newNode.id.replace('XXX', String(currentId));
+        newNode.setAttribute("style", "");
+        elem = newNode;
+    } else {
+        source.removeChild(elem);
+    }
+    if (dest.id != 'recycledBin') {
+        if ((dest.parentNode.className == "group") || (dest.parentNode.className == "groupClass")) {
+            dest.parentNode.insertBefore(elem, dest);
+        } else {
+            dest.appendChild(elem);
+        }
+    }
+    refesh();
+}
